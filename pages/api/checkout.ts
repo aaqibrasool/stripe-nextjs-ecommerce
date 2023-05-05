0ef5020f80c8d9cb79f164ccf2825e41b30e0909
aaqib <aqibrasool10@gmail.com>
2023-05-05 19:06:17 +0500

    removed local host from urls

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -1,46 +1,45 @@
-import {NextApiRequest, NextApiResponse} from "next";
-import {Stripe} from "stripe";
+import { NextApiRequest, NextApiResponse } from "next"
+import { Stripe } from "stripe"
 
 type Res = {
-    session?: Stripe.Checkout.Session
-    message?: string
+  session?: Stripe.Checkout.Session
+  message?: string
 }
 
 type LineItem = {
-    price: string
-    quantity: number
+  price: string
+  quantity: number
 }
 
 type Req = {
-    lineItems: LineItem[]
+  lineItems: LineItem[]
 }
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Res>
+  req: NextApiRequest,
+  res: NextApiResponse<Res>
 ) {
-    if (req.method != 'POST') {
-        res.status(405).json({message: 'POST method required'})
-    }
+  if (req.method != "POST") {
+    res.status(405).json({ message: "POST method required" })
+  }
 
-    try {
-        const body: Req = JSON.parse(req.body);
+  try {
+    const body: Req = JSON.parse(req.body)
 
-        const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
-            apiVersion: '2020-08-27',
-        });
+    const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+      apiVersion: "2020-08-27",
+    })
 
-        const session = await stripe.checkout.sessions.create({
-            success_url: 'http://localhost:3000/success',
-            cancel_url: 'http://localhost:3000/cancel',
-            line_items: body.lineItems,
-            mode: 'payment',
-        });
+    const session = await stripe.checkout.sessions.create({
+      success_url: "/success",
+      cancel_url: "/cancel",
+      line_items: body.lineItems,
+      mode: "payment",
+    })
 
-        res.status(201).json({ session })
-    }catch (e) {
-        // @ts-ignore
-        res.status(500).json({message: e.message})
-    }
-
+    res.status(201).json({ session })
+  } catch (e) {
+    // @ts-ignore
+    res.status(500).json({ message: e.message })
+  }
 }
